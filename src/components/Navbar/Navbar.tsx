744c36bb76f23513adb26ef5c81715b0a486402e
Kleos Dorron <kleos.kd@gmail.com>
2023-12-09 00:21:40 +0530

    Add Student Detail Report Component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Navbar, Nav, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, NavLink, NavItem, UncontrolledDropdown, NavbarBrand } from 'reactstrap';
-import { FaUser, FaBook, FaChalkboardTeacher, FaBuilding, FaFile, FaThList } from 'react-icons/fa';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { Navbar, Nav, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, NavLink, NavItem, UncontrolledDropdown, NavbarBrand } from "reactstrap";
+import { FaUser, FaBook, FaChalkboardTeacher, FaBuilding, FaFile, FaThList } from "react-icons/fa";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 const AppNavbar = () => {
   return (
     <Navbar color="primary" expand="md" dark>
@@ -13,12 +13,19 @@ const AppNavbar = () => {
           src="/BrandLogo.svg"
           style={{
             height: 40,
-            width: 50
+            width: 50,
           }}
         />
         SMS
       </NavbarBrand>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
@@ -34,6 +41,9 @@ const AppNavbar = () => {
           </Link>
           <Link to="/Classrooms" className="nav-link">
             <FaBuilding /> Classrooms
+          </Link>
+          <Link to="/StudentDetailReport" className="nav-link">
+            <FaFile /> Report
           </Link>
           <NavItem>
             <UncontrolledDropdown>
--- src/components/Student-Detail-Report/Student-Detail-Report.tsx
+++ src/components/Student-Detail-Report/Student-Detail-Report.tsx
@@ -1,12 +1,140 @@
-import React, { FC } from 'react';
+import axios from "axios";
+import React, { FC, useEffect, useState } from "react";
+import { Col, Container, Form, FormGroup, Input, Label, Row } from "reactstrap";
 
+interface Student {
+  studentId: number;
+  firstName: string;
+  lastName: string;
+  contactPerson: string;
+  contactNo: string;
+  emailAddress: string;
+  dateOfBirth: string;
+  age: string;
+  classroomId: number;
+}
+const StudentDetailReport: React.FC = () => {
+  const [selectedStudentId, setSelectedStudentId] = useState<number | null>(null);
+  const [students, setStudents] = useState<Student[]>([]);
+  const [studentDetails, setStudentDetails] = useState<Student | null>(null);
+  useEffect(() => {
+    // Fetch students when the component mounts
+    fetchStudents();
+  }, []);
 
-interface StudentDetailReportProps {}
+  const fetchStudents = async () => {
+    try {
+      const response = await axios.get<Student[]>("https://localhost:7016/api/Students");
+      setStudents(response.data);
+    } catch (error) {
+      console.error("Error fetching students:", error);
+    }
+  };
 
-const StudentDetailReport: FC<StudentDetailReportProps> = () => (
-  <div>
-    StudentDetailReport Component
-  </div>
-);
+  const handleStudentChange = async (selectedStudentId: number) => {
+    // Fetch student details based on the selected studentId
+    try {
+      const response = await axios.get<Student>(`https://localhost:7016/api/Students/${selectedStudentId}`);
+      const studentData = response.data;
+
+      // Update the state with the selected student details
+      setStudentDetails(studentData);
+      setSelectedStudentId(selectedStudentId);
+    } catch (error) {
+      console.error("Error fetching student details:", error);
+    }
+  };
+
+  return (
+    <Container fluid="sm" style={{ maxWidth: "900px" }}>
+      <h1 className="mb-4">Student Details</h1>
+
+      <Form>
+        <Row>
+          <Col md={6}>
+            <FormGroup row>
+              <Label for="studentId" sm={4}>
+                Select Student:
+              </Label>
+              <Col sm={8}>
+                <Input type="select" id="studentId" onChange={(e) => handleStudentChange(Number(e.target.value))} value={selectedStudentId || ""}>
+                  <option value="">Select Student</option>
+                  {students.map((student) => (
+                    <option key={student.studentId} value={student.studentId}>
+                      {`${student.firstName} ${student.lastName}`}
+                    </option>
+                  ))}
+                </Input>
+              </Col>
+            </FormGroup>
+
+            <FormGroup row>
+              <Label for="studentName" sm={4}>
+                First Name:
+              </Label>
+              <Col sm={8}>
+                <Input type="text" id="studentName" value={studentDetails?.firstName} readOnly />
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label for="studentName" sm={4}>
+                Last Name:
+              </Label>
+              <Col sm={8}>
+                <Input type="text" id="studentName" value={studentDetails?.lastName} readOnly />
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label for="contactPerson" sm={4}>
+                Contact Person:
+              </Label>
+              <Col sm={8}>
+                <Input type="text" id="contactPerson" value={studentDetails?.contactPerson} readOnly />
+              </Col>
+            </FormGroup>
+          </Col>
+
+          <Col md={6}>
+            <FormGroup row>
+              <Label for="contactNo" sm={4}>
+                Contact No.:
+              </Label>
+              <Col sm={8}>
+                <Input type="text" id="contactNo" value={studentDetails?.contactNo} readOnly />
+              </Col>
+            </FormGroup>
+
+            <FormGroup row>
+              <Label for="emailAddress" sm={4}>
+                Email Address:
+              </Label>
+              <Col sm={8}>
+                <Input type="text" id="emailAddress" value={studentDetails?.emailAddress} readOnly />
+              </Col>
+            </FormGroup>
+
+            <FormGroup row>
+              <Label for="dateOfBirth" sm={4}>
+                Date of Birth:
+              </Label>
+              <Col sm={8}>
+                <Input type="text" id="dateOfBirth" value={studentDetails?.dateOfBirth} readOnly />
+              </Col>
+            </FormGroup>
+
+            <FormGroup row>
+              <Label for="age" sm={4}>
+                Age:
+              </Label>
+              <Col sm={8}>
+                <Input type="text" id="age" value={studentDetails?.age} readOnly />
+              </Col>
+            </FormGroup>
+          </Col>
+        </Row>
+      </Form>
+    </Container>
+  );
+};
 
 export default StudentDetailReport;
